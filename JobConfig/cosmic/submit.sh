cb168d802a1564e4f7189a51e1723931d9fffa62
Yuri Oksuzian <oksuzian@mu2egpvm03.fnal.gov>
2019-09-14 22:53:47 -0500

    Rnamed FirstStepMC

--- JobConfig/cosmic/submit.sh
+++ JobConfig/cosmic/submit.sh
@@ -7,30 +7,27 @@ setup mu2etools
 setup gridexport
 setup dhtools
 
-
 MAINDIR=`pwd`
 MAKEFLC=0
 #STAGE=stage1
-#STAGE=minedep_filter
-STAGE=dsvacuum_resampler
+STAGE=minedep_filter
+#STAGE=dsvacuum_resampler
 #STAGE=digi
 #STAGE=reco
 
-
-
 if [ "$STAGE" == "dsvacuum_analyzer" ]; then
     INLIST=submissionLists/beams1_0619_dsregion_filelist.txt
-    INFCL=Mu2eANL/Analyses/fcl/StepPointMC1stHitDumper_analyzer.fcl
+    INFCL=Mu2eANL/Analyses/fcl/analyzeFirstStepMC.fcl
     TAG=
 
 elif [ "$STAGE" == "stage1" ]; then
-    INFCL=Mu2eANL/JobConfig/cosmic/cosmic_s1_general_cryMT_DSVaccum.fcl
+    INFCL=Mu2eANL/JobConfig/cosmic/cosmic_s1_general.fcl
     TAG=190909133703
 
 elif [ "$STAGE" == "minedep_filter" ]; then
     INLIST=Mu2eANL/JobConfig/FileLists/cry0919_1k_round2.txt
-    INFCL=Mu2eANL/Filters/fcl/FilterMinEdepCRV_filter.fcl
-    TAG=190911085549
+    INFCL=Mu2eANL/JobConfig/cosmic/filterGapEvents.fcl
+    TAG=190914223923
 
 elif [ "$STAGE" == "dsvacuum_resampler" ]; then
     INFCL=Mu2eANL/JobConfig/cosmic/dsvacuum_resampler.fcl
@@ -53,7 +50,7 @@ fi
 DSCONF=cry${STAGE}_0919
 SETUPFN=${MU2E_BASE_RELEASE}/setup.sh
 WFPROJ=cry_0919
-CODE=/pnfs/mu2e/resilient/users/oksuzian/gridexport/tmp.GxB7O2hbiS/Code.tar.bz
+CODE=/pnfs/mu2e/resilient/users/oksuzian/gridexport/tmp.VmnM4jYMtD/Code.tar.bz
 
 OUTDIR=${DSCONF}_${TAG}
 
@@ -71,7 +68,7 @@ if [ "$STAGE" == "dsvacuum_analyzer" ]; then
 elif [ "$STAGE" == "stage1" ]; then
     (cd backup/$OUTDIR && generate_fcl --desc=sim --dsowner=oksuzian --dsconf=$DSCONF --njobs=2000 --events-per-job=5000000 --run-number=2700 --embed ${MAINDIR}/${INFCL})
 elif [ "$STAGE" == "minedep_filter" ]; then
-    (cd backup/$OUTDIR && generate_fcl --desc=sim --dsowner=oksuzian --dsconf=$DSCONF --inputs=${MAINDIR}/${INLIST} --merge=20 --embed ${MAINDIR}/${INFCL})
+    (cd backup/$OUTDIR && generate_fcl --desc=sim --dsowner=oksuzian --dsconf=$DSCONF --inputs=${MAINDIR}/${INLIST} --merge=50 --embed ${MAINDIR}/${INFCL})
 elif [ "$STAGE" == "dsvacuum_resampler" ]; then
     (cd backup/$OUTDIR && generate_fcl --desc=sim --dsowner=oksuzian --dsconf=$DSCONF --njobs=500 --events-per-job=526000 --run=2701 --embed ${MAINDIR}/${INFCL}  --aux=1:physics.filters.dsResample.fileNames:${MAINDIR}/Mu2eANL/JobConfig/FileLists/cry0919_minedep-filter.nocat.1k_round2.txt)
 elif [ "$STAGE" == "digi" ]; then
@@ -106,10 +103,8 @@ if [ "$STAGE" == "stage1" ] || [ "$STAGE" == "dsvacuum_resampler" ]; then
     RESOURCE="--disk=20GB --memory=3500MB"
 fi
 
-#   --dsowner=oksuzian --OS=SL7 ${RESOURCE} --expected-lifetime=23h --code=$CODE \
-
 command="mu2eprodsys --clustername="${JN}" --fcllist=$SF --wfproject=$WFPROJ --dsconf=$DSCONF \
-   --dsowner=oksuzian --OS=SL7 ${RESOURCE} --expected-lifetime=23h --setup=/cvmfs/mu2e.opensciencegrid.org/Offline/v7_5_4/SLF7/prof/Offline/setup.sh \
+   --dsowner=oksuzian --OS=SL7 ${RESOURCE} --expected-lifetime=23h --code=$CODE \
    --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC"
 echo "Submitting: " $command
 echo `$command` > $LN 2>&1
