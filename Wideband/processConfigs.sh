f9d6e64976b21232a4f4d6305261155c3cbcc3f5
Yuri Oksuzian <oksuzian@gmail.com>
2023-07-03 05:48:52 +0000

    Changes for analytics-hub

--- Wideband/processConfigs.sh
+++ Wideband/processConfigs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+input_dir="/home/oksuzian/Mu2eANL/Wideband"
+output_dir="/home/oksuzian/Mu2eANL/Wideband/aging_nb"
+web_dir="/web/sites/mu2e.fnal.gov/htdocs/atwork/workgroups/Wideband"
+kernel="MyEnv"
+
 for suffix in PE SPE Aging Temp; do
     for config in 001ext 006 007; do
  
         output_file="${suffix}-${config}"
         output_htmlfile="${suffix}-${config}.html"
-        output_dir="/mu2e/app/users/oksuzian/Mu2eANL/Wideband"
-        web_dir="/web/sites/mu2e.fnal.gov/htdocs/atwork/workgroups/Wideband"
-        dataset="rec.mu2e.CRV_wideband_cosmics.CRVWB-000-001-000-${config}.root"
 
-        papermill ${output_dir}/${suffix}.ipynb ${output_dir}/${output_file}.ipynb -p DATASET "${dataset}"
-        jupyter nbconvert --to html ${output_dir}/${output_file}.ipynb --output-dir ${web_dir} --no-input
+        dataset="rec.mu2e.CRV_wideband_cosmics.CRVWB-000-003-000-${config}.root"
+        papermill ${input_dir}/${suffix}.ipynb ${output_dir}/${output_file}.ipynb -p DATASET "${dataset}" -k $kernel
+        jupyter nbconvert --to html ${output_dir}/${output_file}.ipynb --output-dir ${output_dir} --no-input
         date=$(date +%Y-%m-%d)
-        cp ${web_dir}/${output_file}.html  ${web_dir}/${output_file}_${date}.html
+        scp ${output_dir}/${output_file}.html mu2egpvm03.fnal.gov:/${web_dir}/${output_file}.html
+        scp ${output_dir}/${output_file}.html mu2egpvm03.fnal.gov:/${web_dir}/${output_file}_${date}.html
 
     done
 done
