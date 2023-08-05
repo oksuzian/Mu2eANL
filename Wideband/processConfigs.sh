53d64f404ef686a54d838ce6e75acb79409d4e1e
Yuri Oksuzian <oksuzian@gmail.com>
2023-08-05 15:29:59 +0000

    Processed with V8

--- Wideband/processConfigs.sh
+++ Wideband/processConfigs.sh
@@ -5,13 +5,13 @@ output_dir="/home/oksuzian/Mu2eANL/Wideband/aging_nb"
 web_dir="/web/sites/mu2e.fnal.gov/htdocs/atwork/workgroups/Wideband"
 kernel="MyEnv"
 
-for suffix in PE SPE Aging Temp; do
+for suffix in Aging Temp PE SPE; do
     for config in 001ext 006 007; do
  
         output_file="${suffix}-${config}"
         output_htmlfile="${suffix}-${config}.html"
 
-        dataset="rec.mu2e.CRV_wideband_cosmics.CRVWB-000-003-000-${config}.root"
+        dataset="rec.mu2e.CRV_wideband_cosmics.CRVWB-000-004-000-${config}.root"
         papermill ${input_dir}/${suffix}.ipynb ${output_dir}/${output_file}.ipynb -p DATASET "${dataset}" -k $kernel
         jupyter nbconvert --to html ${output_dir}/${output_file}.ipynb --output-dir ${output_dir} --no-input
         date=$(date +%Y-%m-%d)
